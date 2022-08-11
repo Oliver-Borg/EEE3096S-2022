2dd98f118317b0a91679122ff8de4b3e01f7f490
Oliver Borg <brgoli005@cs.uct.ac.za>
2022-08-11 11:59:13 +0200

    Added functionality for accuracy data

--- WorkPackage1/convert.py
+++ WorkPackage1/convert.py
@@ -1,6 +1,6 @@
 with open("results.txt", 'r') as f:
     a = f.readlines()
-headings = ['Language', 'Threads', 'Optimisation', 'Bit Width', 'Time', 'Run', 'Unroll']
+headings = ['Language', 'Threads', 'Optimisation', 'Bit Width', 'Time', 'Run', 'Unroll', 'Accuracy']
 with open("results.csv", 'w') as f:
     f.write(','.join(headings))
     i = 0
@@ -9,25 +9,24 @@ with open("results.csv", 'w') as f:
     optimiser = ''
     unroll = False
     width = ''
-    thread_index = -1
-    threads = [2, 4, 8, 16, 32, 1]
+    threads = ''
+    acc = ''
     while(i < len(a)):
         if 'Elapsed time' in a[i]:
-            f.write('\nPython, 0, None, float, ' + str(float(a[i][22:].strip())/1000) + f', {run}, False')
+            f.write('\nPython, 0, None, float, ' + str(float(a[i][22:].strip())/1000) + f', {run}, False', '100.0')
             run+=1
         if 'CFLAGS' in a[i]:
-            if 'unthreaded' not in a[i-1]:
-                thread_index = (thread_index+1)%6
             flags = a[i][a[i].index('-O'):]
             optimiser = flags[flags.index('-O'):flags.index(' ')]
             unroll = 'unroll' in flags[flags.index(' '):] 
             run = 1
             width = a[i+1].strip()
+            threads = a[i+2].split(' ')[2].strip()
         if 'Time:' in a[i]:
             f.write(f'\nC, 0, {optimiser}, {width}, ' + a[i][6:14].strip() + f', {run}, {unroll}')
             run+=1
         elif 'Time taken' in a[i]:
-            f.write(f'\nC, {threads[thread_index]}, {optimiser}, {width}, ' + a[i].split(' ')[7].strip() + f', {run}, {unroll}')
+            f.write(f'\nC, {threads}, {optimiser}, {width}, ' + a[i].split(' ')[7].strip() + f', {run}, {unroll}, {acc}')
             run+=1
 
         i+=1
