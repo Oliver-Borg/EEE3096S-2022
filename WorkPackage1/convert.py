2e758f284aec2da48ad7c34ef8fdce82603a6567
Oliver Borg <brgoli005@cs.uct.ac.za>
2022-08-11 19:19:19 +0200

    Fixed conversion bug

--- WorkPackage1/convert.py
+++ WorkPackage1/convert.py
@@ -1,4 +1,4 @@
-with open("results.txt", 'r') as f:
+with open("results/results1660231371.txt", 'r') as f:
     a = f.readlines()
 headings = ['Language', 'Threads', 'Optimisation', 'Bit Width', 'Time', 'Run', 'Unroll', 'Accuracy']
 with open("results.csv", 'w') as f:
@@ -13,7 +13,7 @@ with open("results.csv", 'w') as f:
     acc = ''
     while(i < len(a)):
         if 'Elapsed time' in a[i]:
-            f.write('\nPython, 0, None, float, ' + str(float(a[i][22:].strip())/1000) + f', {run}, False', '100.0')
+            f.write('\nPython, 0, None, float, ' + str(float(a[i][22:].strip())/1000) + f', {run}, False, 100.0')
             run+=1
         if 'CFLAGS' in a[i]:
             flags = a[i][a[i].index('-O'):]
@@ -21,7 +21,8 @@ with open("results.csv", 'w') as f:
             unroll = 'unroll' in flags[flags.index(' '):] 
             run = 1
             width = a[i+1].strip()
-            threads = a[i+2].split(' ')[2].strip()
+            if "Thread_Count" in a[i+2]:
+                threads = a[i+2].split(' ')[2].strip()
         if 'Time:' in a[i]:
             f.write(f'\nC, 0, {optimiser}, {width}, ' + a[i][6:14].strip() + f', {run}, {unroll}')
             run+=1
