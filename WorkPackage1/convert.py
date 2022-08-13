42590b5b2ad55f6da0ab9d09b21dcd56fc51f1ba
Oliver Borg <brgoli005@cs.uct.ac.za>
2022-08-13 18:40:23 +0200

    Allowed convert to read memory use

--- WorkPackage1/convert.py
+++ WorkPackage1/convert.py
@@ -1,6 +1,6 @@
-with open("results/results1660247500.txt", 'r') as f:
+with open('results/results1660404422.txt', 'r') as f:
     a = f.readlines()
-headings = ['Language', 'Threads', 'Optimisation', 'Bit Width', 'Time', 'Run', 'Unroll', 'Accuracy']
+headings = ['Language', 'Threads', 'Optimisation', 'Bit Width', 'Time', 'Run', 'Unroll', 'Accuracy', 'Memory']
 with open("results.csv", 'w') as f:
     f.write(','.join(headings))
     i = 0
@@ -11,9 +11,14 @@ with open("results.csv", 'w') as f:
     width = ''
     threads = ''
     acc = ''
+    mem=''
     while(i < len(a)):
         if 'Elapsed time' in a[i]:
-            f.write('\nPython, 0, None, float, ' + str(float(a[i][22:].strip())/1000) + f', {run}, False, 16')
+            mem = a[i+2].split(' ') 
+            while('' in mem):
+                mem.remove('')
+            mem = mem[2].replace(',', '').strip()
+            f.write('\nPython, 0, None, float, ' + str(float(a[i][22:].strip())/1000) + f', {run}, False, 16, {mem}')
             run+=1
         if 'CFLAGS' in a[i]:
             flags = a[i][a[i].index('-O'):]
@@ -25,11 +30,20 @@ with open("results.csv", 'w') as f:
                 threads = a[i+2].split(' ')[2].strip()
         if 'Accuracy' in a[i]:
             acc = a[i+1].strip()
+            
         if 'Time:' in a[i]:
-            f.write(f'\nC, 0, {optimiser}, {width}, ' + a[i][6:14].strip() + f', {run}, {unroll}, {acc}')
+            mem = a[i+2].split(' ')
+            while('' in mem):
+                mem.remove('')
+            mem = mem[2].replace(',', '').strip()
+            f.write(f'\nC, 0, {optimiser}, {width}, ' + a[i][6:14].strip() + f', {run}, {unroll}, {acc}, {mem}')
             run+=1
         elif 'Time taken' in a[i]:
-            f.write(f'\nC, {threads}, {optimiser}, {width}, ' + a[i].split(' ')[7].strip() + f', {run}, {unroll}, {acc}')
+            mem = a[i+1].split(' ')
+            while('' in mem):
+                mem.remove('')
+            mem = mem[2].replace(',', '').strip()
+            f.write(f'\nC, {threads}, {optimiser}, {width}, ' + a[i].split(' ')[7].strip() + f', {run}, {unroll}, {acc}, {mem}')
             run+=1
 
         i+=1
